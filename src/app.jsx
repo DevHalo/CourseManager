544409efc3e4c22cd8167ddf4474189cfad025d5
Mark Voong <devhalo@hotmail.com>
2019-05-30 21:38:18 -0400

    React experimentation

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Button } from "@blueprintjs/core";
+import TestComponent from './components/TestComponent';
 
 export default class App extends React.Component {
   render() {
     return (<div>
       <h2>Welcome to React!</h2>
-      <Button onClick={this.test} />
+      <Button onClick={this.test}>asdasd</Button>
+      <TestComponent />
     </div>
     );
   }
---
+++ src/components/TestComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class TestComponent extends React.Component {
+  render() {
+    return (
+        <div>
+            <h2>This is a test component</h2>
+        </div>
+    );
+  }
+}
